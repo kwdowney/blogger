127c0627718f7efc07129ec14f815aec78a13008
Kyle Downey <kylewdowney@gmail.com>
2017-01-05 19:33:53 -0800

    Tagging feature completed

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,28 @@
 class Article < ApplicationRecord
 	has_many :comments
+	has_many :taggings
+	has_many :tags, through: :taggings
+
+	def tag_list
+  		self.tags.collect do |tag|
+    	tag.name
+  		end.join(", ")
+	end
+
+	def tag_list=(tags_string)
+
+	#Split the tags_string into an array of strings with leading and trailing whitespace removed	
+		tag_names = tags_string.split(",").collect{|s| s.strip.downcase}.uniq
+  	
+  	
+    #Ensure each one of these strings are unique
+    #Look for a Tag object with that name. If there isn’t one, create it.
+    #Add the tag object to a list of tags for the article
+  	
+  		new_or_found_tags = tag_names.collect { |name| Tag.find_or_create_by(name: name) }
+  	
+  	#Set the article’s tags to the list of tags that we have found and/or created	
+  		self.tags = new_or_found_tags
+	end
+
 end
